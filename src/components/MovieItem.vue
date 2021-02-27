8ee2ebbc6d2b72778db17ad509988dc07c4695be
lemokh <byndasma@gmail.com>
2021-02-27 08:53:02 +0400

    MovieItem.vue

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="movie">
     <div class="movie-col-left">
-      <img v-bind:src="movie.Poster" />
+      <router-link v-bind:to="{ name: 'movie', params: { id: movie.imdbID } }">
+        <img v-bind:src="movie.Poster" />
+      </router-link>
     </div>
     <div class="movie-col-right">
       <div class="movie-title">
-        <h2>{{ movie.Title }}</h2>
+        <router-link v-bind:to="{ name: 'movie', params: { id: movie.imdbID } }">
+          <h2>{{ movie.Title }}</h2>
+        </router-link>
         <span class="movie-rating">{{ movie.Rated }}</span>
       </div>
+      <slot></slot>
     </div>
   </div>
 </template>
